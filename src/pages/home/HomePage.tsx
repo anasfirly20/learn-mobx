608fa72cbe7719df83405b904c9f3229d065e71d
Anas Firly <anasfirly20@gmail.com>
2023-08-29 10:17:06 +0400

    adding dependencies

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(): React.JSX.Element {
+  return (
+    <div>
+      <h1>HOMEPAGE</h1>
+    </div>
+  );
+}
---
+++ src/pages/todo/TodoPage.tsx
@@ -0,0 +1,7 @@
+export default function TodoPage(): React.JSX.Element {
+  return (
+    <div>
+      <h1>TODO</h1>
+    </div>
+  );
+}
